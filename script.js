8b10c225ee16be8f5d5142a5bdbe921285b3ac8a
Yashtyagi1504 <166196060+Yashtyagi1504@users.noreply.github.com>
2025-01-30 03:02:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function insert_Row() {
     //Write your code here
 	let table = document.querySelector("#sampleTable");
-	table.innerHtml = "<tr><td>New Cell1</td> <td>New Cell2</td></tr>"
+	table.innerHTML = "<tr><td>New Cell1</td> <td>New Cell2</td></tr>"
   
   
 }
