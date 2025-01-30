7c08e0dcd0af781935718348743c30c94eb36465
Yashtyagi1504 <166196060+Yashtyagi1504@users.noreply.github.com>
2025-01-31 02:17:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function insert_Row() {
     //Write your code here
 	let table = document.querySelector("#sampleTable");
-	let row = table.insertRow();
+	let row = table.insertRow(0);
 	for(let i = 0;i<2;i++){
 		let cell = row.insertCell(i);
 		cell.innerHTML = `New Cell ${i+1}`
