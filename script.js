a0b0ad4e1e912fa9537d8cc63001399d7113aaac
Yashtyagi1504 <166196060+Yashtyagi1504@users.noreply.github.com>
2025-01-31 02:13:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function insert_Row() {
 	let table = document.querySelector("#sampleTable");
 	let row = table.insertRow();
 	for(let i = 0;i<2;i++){
-		let `cell ${i+1}` = row.insertCell(i);
-		`cell ${i+1}`.innerHTML = `New Cell ${i+1}`
+		let cell = row.insertCell(i);
+		cell.innerHTML = `New Cell ${i+1}`
 	}
 }
